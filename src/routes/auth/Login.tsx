96217271ff1109871d4edb80330b80632f6d903e
Your Name <you@example.com>
2022-03-26 23:14:27 +0430

    use formik and yup to validate form data

--- src/routes/auth/Login.tsx
+++ src/routes/auth/Login.tsx
@@ -1,21 +1,38 @@
 import {SignInIcon} from '@primer/octicons-react'
+import { Button } from 'components/formElements/Button';
+import { InputElement } from 'components/formElements/InputElement';
+import { useFormik } from 'formik';
+import { loginFormSchema } from 'models/validationSchema/authSchema';
 import { Link } from 'react-router-dom';
 export const Login=()=>{
+
+    const formik=useFormik({
+        initialValues:{
+            username:"",
+            password:"",
+        },
+        validateOnChange:false,
+        validationSchema:loginFormSchema,
+        onSubmit:values=>console.log(values)
+    })
+    
     return(
         <div className="container mx-auto mt-28 px-4">
-            <div className='w-full lg:w-96 md:w-4/6 bg-white p-5 text-center mx-auto rounded-md'>
-                <SignInIcon  size={44} className="block h-12 w-12 bg-yellow-300 rounded-full text-black mb-4"/>
+            <div className='w-full lg:w-96 md:w-4/6 bg-white p-5 mx-auto rounded-md'>
+                <div className='text-center'>
+                    <SignInIcon  size={44} className="block h-12 w-12 bg-yellow-300 rounded-full text-black mb-4"/>
+                </div>
                 <h1 className="font-bold text-gray-500 text-center">LOG IN TO YOUR ACCOUNT</h1>
-                <div className='mt-4'>
-                <input type="text" className='input rounded-md bg-gray-100 w-full' placeholder='Username (only string)' />
-                </div>
-                <div className='mt-4'>
-                <input type="text" className='input rounded-md bg-gray-100 w-full' placeholder='Password (min 6 char)' />
-                </div>
-                <button className='btn btn-block my-5 bg-violet-500 hover:bg-violet-800 border-0 '>LOG IN</button>
+                <form onSubmit={formik.handleSubmit}>
+
+                    <InputElement name='username' value={formik.values.username} onChange={formik.handleChange} errorMsg={formik.errors.username} type="text" key={1} placeholder='Username (At least 6 characters)'/>
+                    
+                    <InputElement name='password' value={formik.values.password} onChange={formik.handleChange} errorMsg={formik.errors.password} type="password" key={2} placeholder='Password (At least 6 characters)'/>
+                    
+                    <Button type={'submit'} isLoading={formik.isSubmitting} fullWidth={true} >Logn In</Button>
+                </form>
                 <p className='text-gray-400 text-sm text-center mt-2 mb-2 '>Don't have account ? <Link to="/auth/register" className='text-violet-900 '>Register Now</Link></p>
             </div>
-
-      </div>
+        </div>
     );
 }
