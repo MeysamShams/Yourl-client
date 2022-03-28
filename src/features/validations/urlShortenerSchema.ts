789e2d42bb2d3c4c0b548fabb24576fd2d32c1af
Your Name <you@example.com>
2022-03-29 00:25:20 +0430

    improve regex pattern

--- src/features/validations/urlShortenerSchema.ts
+++ src/features/validations/urlShortenerSchema.ts
@@ -1,5 +1,5 @@
 import * as Yup from 'yup'
 
 export const urlShortenerSchema=Yup.object().shape({
-    url:Yup.string().matches(/^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/,"Please enter a valid URL !").required("URL is a required field !")
+    url:Yup.string().matches(/^(?:http(s)?:\/\/)[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/,"Please enter a valid URL !").required("URL is a required field !")
 })
