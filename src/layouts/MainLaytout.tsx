ad34cc3802b5c265d6fe0d3bec5f2ce213f55983
Your Name <you@example.com>
2022-03-29 14:38:11 +0430

    move ToastContainer from MainLayout to the App

--- src/layouts/MainLaytout.tsx
+++ src/layouts/MainLaytout.tsx
@@ -1,8 +1,6 @@
 import { Navbar } from "components/main/navbar/Navbar"
 import { Route, Routes } from "react-router-dom"
 import { Home } from "pages/Home"
-import { ToastContainer } from "react-toastify"
-import 'react-toastify/dist/ReactToastify.css';
 import { Auth } from "pages/Auth"
 
 export const MainLayout=()=>{
@@ -12,16 +10,5 @@ export const MainLayout=()=>{
             <Route path="/" element={<Home/>}/>
             <Route path="/auth/*" element={<Auth/>}/>
         </Routes>
-        <ToastContainer
-            position="top-right"
-            autoClose={5000}
-            hideProgressBar={false}
-            newestOnTop={false}
-            closeOnClick
-            rtl={false}
-            pauseOnFocusLoss
-            draggable
-            pauseOnHover
-        />
     </>
 }
